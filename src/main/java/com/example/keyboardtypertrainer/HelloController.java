6cd95cc3db005346dcea58b590ba7941a890037f
Roman <prostopostacuba@gmail.com>
2023-03-06 23:07:08 +0200

    Added base elements

--- src/main/java/com/example/keyboardtypertrainer/HelloController.java
+++ src/main/java/com/example/keyboardtypertrainer/HelloController.java
@@ -2,13 +2,17 @@ package com.example.keyboardtypertrainer;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.input.KeyEvent;
 
 public class HelloController {
     @FXML
-    private Label welcomeText;
+    public Label mylabel;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    public TextArea myTextArea;
+
+    public void textAreaTyped(KeyEvent event){
+        System.out.println(myTextArea.getText());
     }
 }
