9d32dafc3e480663afe516b606a5189482f5b94a
Roman <prostopostacuba@gmail.com>
2023-03-07 22:33:45 +0200

    added base funcs

--- src/main/java/com/roman/keyboardtypertrainer/Controller.java
+++ src/main/java/com/roman/keyboardtypertrainer/Controller.java
@@ -5,7 +5,7 @@ import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
 import javafx.scene.input.KeyEvent;
 
-public class HelloController {
+public class Controller {
     @FXML
     public Label mylabel;
 
